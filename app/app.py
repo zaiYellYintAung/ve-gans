19b50bc1807ec304123c0264d171b36fc2b12b18
Zai <130903099+zaiYellYintAung@users.noreply.github.com>
2023-12-25 23:53:38 +0630

    prototype done

--- app/app.py
+++ app/app.py
@@ -1,1 +1,12 @@
-# stream lit app.py
+import streamlit as st
+import torch
+from torchvision.utils import make_grid
+from torchvision.transforms import ToPILImage
+
+def main():
+    st.title("Image Generation")
+    st.write("Made with GANS from scratch")
+    
+    
+if __name__ == '__main__':
+    main()
